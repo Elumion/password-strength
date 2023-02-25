ec7ca1ae6a0e20f530485f4ac1a1e3247d646b3a
Elumion <dovgalyukvlad87@gmail.com>
2023-02-25 13:30:43 +0200

    create concept of sections

---
+++ src/components/PasswordStrengthSections/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+export default function Sections() {
+  return (
+    <div className="password-sections__container">
+      <span className="password-section easy"></span>
+      <span className="password-section medium"></span>
+      <span className="password-section hard"></span>
+    </div>
+  );
+}
