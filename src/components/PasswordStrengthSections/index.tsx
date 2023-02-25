6f9359ec172456487fc3c2781a3b42790af66dfd
Elumion <dovgalyukvlad87@gmail.com>
2023-02-25 22:48:43 +0200

    working concept of component

--- src/components/PasswordStrengthSections/index.tsx
+++ src/components/PasswordStrengthSections/index.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./index.css";
+import { SectionsStrength } from "./index.types";
 
-export default function Sections() {
+interface Props {
+  strengthValue?: SectionsStrength;
+}
+
+function resetClass(el: Element) {
+  el.setAttribute("class", "password-section");
+}
+
+export default function Sections({ strengthValue }: Props) {
+  const sectionsContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (sectionsContainerRef) {
+      const sectionsArr = [...sectionsContainerRef.current?.children!];
+
+      if (strengthValue === SectionsStrength.EMPTY) {
+        sectionsArr.forEach((el) => {
+          resetClass(el);
+        });
+      } else if (strengthValue === SectionsStrength.SHORT) {
+        sectionsArr.forEach((el) => {
+          resetClass(el);
+          el.classList.add(SectionsStrength.SHORT);
+        });
+      } else if (strengthValue === SectionsStrength.EASY) {
+        sectionsArr.forEach((el, index) => {
+          resetClass(el);
+          if (index < 1) el.classList.add(SectionsStrength.EASY);
+        });
+      } else if (strengthValue === SectionsStrength.MEDIUM) {
+        sectionsArr.forEach((el, index) => {
+          resetClass(el);
+          if (index < 2) el.classList.add(SectionsStrength.MEDIUM);
+        });
+      } else if (strengthValue === SectionsStrength.HARD) {
+        sectionsArr.forEach((el, index) => {
+          resetClass(el);
+          if (index < 3) el.classList.add(SectionsStrength.HARD);
+        });
+      }
+    }
+  }, [strengthValue]);
+
   return (
-    <div className="password-sections__container">
-      <span className="password-section easy"></span>
-      <span className="password-section medium"></span>
-      <span className="password-section hard"></span>
+    <div ref={sectionsContainerRef} className="password-sections__container">
+      <span className="password-section "></span>
+      <span className="password-section "></span>
+      <span className="password-section "></span>
     </div>
   );
 }
