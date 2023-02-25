49029afe5b5feec70ad1c7c03fcc29b1343d3d65
Elumion <dovgalyukvlad87@gmail.com>
2023-02-25 13:30:25 +0200

    add sections component and styles

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import "./index.css";
+import Sections from "../PasswordStrengthSections";
 
 export default function PasswordInput() {
-  return <div>PasswordInput</div>;
+  return (
+    <div>
+      <span className="password-input__container">
+        <input
+          className="password-input"
+          type="password"
+          placeholder="Password"
+        />
+      </span>
+      <div className="sections__container">
+        <Sections />
+      </div>
+    </div>
+  );
 }
